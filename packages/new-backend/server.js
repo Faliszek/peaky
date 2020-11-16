e16267d582a7ed8fb375a161cc418be37bfca7aa
Paweł Falisz <pawlic7@gmail.com>
2020-11-16 08:51:41 +0100

    Init backend with bs-express

---
+++ packages/new-backend/server.js
@@ -0,0 +1,37 @@
+var express = require("express");
+var { graphqlHTTP } = require("express-graphql");
+var { buildSchema } = require("graphql");
+
+// Construct a schema, using GraphQL schema language
+var schema = buildSchema(`
+  type Query {
+    quoteOfTheDay: String
+    random: Float!
+    rollThreeDice: [Int]
+  }
+`);
+
+// The root provides a resolver function for each API endpoint
+var root = {
+  quoteOfTheDay: () => {
+    return Math.random() < 0.5 ? "Take it easy" : "Salvation lies within";
+  },
+  random: () => {
+    return Math.random();
+  },
+  rollThreeDice: () => {
+    return [1, 2, 3].map((_) => 1 + Math.floor(Math.random() * 6));
+  },
+};
+
+var app = express();
+app.use(
+  "/graphql",
+  graphqlHTTP({
+    schema: schema,
+    rootValue: root,
+    graphiql: true,
+  })
+);
+app.listen(4000);
+console.log("Running a GraphQL API server at localhost:4000/graphql");
---
+++ packages/new-backend/src/Index.re
@@ -0,0 +1,1 @@
+let _ = Js.log("Hello, BuckleScript");
